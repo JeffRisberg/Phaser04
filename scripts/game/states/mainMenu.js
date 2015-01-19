a861b9ee1542c2358072c36dfec6f2471b9d8685
Jeff Risberg <jrisberg@aol.com>
2015-01-18 22:22:49 -0800

    simplified buttons

--- scripts/game/states/mainMenu.js
+++ scripts/game/states/mainMenu.js
@@ -9,14 +9,14 @@ define(function () {
     MainMenu.prototype = {
         preload: function () {
             this.load.image('background', 'media/backgrounds/mainMenu.png');
-            this.load.spritesheet('button-start', 'media/buttons/button-start.png', 401, 143);
+            this.load.spritesheet('btn-game-start', 'media/buttons/btn-game-start.png', 401, 143);
         },
 
         create: function () {
             this.input.keyboard.createCursorKeys();
             this.background = this.add.tileSprite(0, 0, game.width, game.height, 'background');
 
-            this.add.button(580, 200, 'button-start', this.startGame, this, 1, 0, 2);
+            this.add.button(580, 200, 'btn-game-start', this.startGame, this, 1, 0, 2);
         },
 
         update: function () {
